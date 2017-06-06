0f4da3b07491b492916e9c73378491a7a6f48b13
katrine911 <kate.efremova@gmail.com>
2017-06-06 14:52:07 +0300

    Update client.cs

--- client.cs
+++ client.cs
@@ -2,7 +2,6 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
-
 namespace Client
 {
     class Program
@@ -27,38 +26,20 @@ namespace Client
         {
 
             byte[] bytes = new byte[1024];
-
-
-
-
             IPHostEntry ipHost = Dns.GetHostEntry("192.168.0.102");
             IPAddress ipAddr = Array.FindAll(ipHost.AddressList, a => a.AddressFamily == AddressFamily.InterNetwork)[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
-
             Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-
             sender.Connect(ipEndPoint);
-
             Console.Write("Send MEssage: ");
             string message = Console.ReadLine();
-
             Console.WriteLine("\nSocet connect with {0} ", sender.RemoteEndPoint.ToString());
             byte[] msg = Encoding.UTF8.GetBytes(message);
-
-
             int bytesSent = sender.Send(msg);
-
-
             int bytesRec = sender.Receive(bytes);
-
             Console.WriteLine("\nServer answer: {0}\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
-
-
             if (message.IndexOf("exit") == -1)
                 SendMessageFromSocket(port);
-
-
             sender.Shutdown(SocketShutdown.Both);
             sender.Close();
         }
