0e48f9e2311e59a4f33127fc0778c9dee951a99d
katrine911 <kate.efremova@gmail.com>
2017-06-07 12:42:01 +0300

    Update Server.cs

--- Server.cs
+++ Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,27 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 namespace server
 {
-    class Program
+    class Server
     {
-        static void Main(string[] args) {
-            TcpListener serverSocket = new TcpListener(1255);
-            serverSocket.Start();
+        static void Main(string[] args)
+        {
+            UdpClient socket = new UdpClient(1255);
+            IPEndPoint senderEndPoint = null;
             Console.WriteLine("Сервер запущен");
             while (true) {
-                try {
-                    TcpClient socket = serverSocket.AcceptTcpClient();
-                    NetworkStream nwStream = socket.GetStream();
-                    BinaryReader reader = new BinaryReader(nwStream);
-                    BinaryWriter writer = new BinaryWriter(nwStream);
-                    string filePath = reader.ReadString();
-                    Console.WriteLine("Запрошена информация о файле " + filePath);
-                    FileInfo file = new FileInfo(filePath);
-                    string response = file.Exists ? file.FullName + ":\nДата создания: " + file.CreationTime + "\nРазмер: " + file.Length + " байт" : "Файл не найден";
-                    writer.Write(response);
-                    socket.Close();
-                } catch(Exception e) { Console.WriteLine(e.Message); }
+                byte[] buffer = socket.Receive(ref senderEndPoint);
+                string filePath = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine("Запрошена информация о файле " + filePath);
+                FileInfo file = new FileInfo(filePath);
+               
+                string response = file.Exists ? file.FullName + ":\nДата создания: " + file.CreationTime + "\nРазмер: " + file.Length + " байт": "Файл не найден";
+                byte[] bytes = Encoding.UTF8.GetBytes(response);
+                socket.Send(bytes, bytes.Length, senderEndPoint);
             }
-            serverSocket.Stop();
             Console.ReadKey();
         }
     }
